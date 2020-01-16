f8fcdda2bcec84021192676c8a01045f67241f66
Quinn James <qj@quinnjam.es>
2020-01-16 17:00:02 -0500

    New render system. I believe the math is correct in these cases, just trying to find a valid camera matrix at this point.

---
+++ .old_opengl/main.py
@@ -0,0 +1,78 @@
+from PIL import Image
+from bounding_box import *
+from render import *
+
+##########################################################
+
+# Global constants
+BOX_TYPES = (("Car", (1.7, 2.0, 5.0), (1, 0, 0)),  # (name, dimensions(h,w,l), color_value)
+             ("Cyclist", (1.8,0.5,2.0), (0,1,0)),
+             ("Pedestrian", (1.7,0.5,0.5), (0,0,1)))
+INPUT_FILE_NAME = "image_2/img*.jpg"
+NUMBER_OF_FRAMES = 1500
+OUTPUT_FILE_NAME = "output.txt"
+
+##########################################################
+
+# Sets the background image of the render process to the current frame
+def change_frame():
+    set_background_image(frames[current_frame])
+
+
+# Adds a bounding box to the current frame based on several parameters
+def instantiate_box(type, dimensions, location, rotation_y, color_value):
+    # Constructor: (type, truncated, occluded, alpha, bbox, dimensions, location, rotation_y, color_value)
+    new_box = BoundingBox(type, 0, 0, 0, [0,0,0,0], dimensions, location, rotation_y, color_value)
+    global selected_box
+    selected_box = len(boxes)
+    boxes.append(new_box)
+
+
+# Loads a given number of frames following a naming convention of type "*.png" where * is replaced by integers from
+# 000001 to number_of_frames (each integer padded to 6 decimal places)
+def setup_frames(number_of_frames, naming_convention):
+    print("Loading " + str(number_of_frames) + " images of type " + naming_convention)
+    for i in range(1,number_of_frames+1):
+        f_index = str(i)
+        while len(f_index) < 6:  # Padding
+            f_index = "0" + f_index
+        pil_image = Image.open(naming_convention.replace("*",f_index))
+        frames.append(pil_image)
+    print ("Loading complete")
+
+# Global variables
+current_frame = 0
+frames = []
+boxes = []
+
+
+# Initial setup
+setup_frames(NUMBER_OF_FRAMES, INPUT_FILE_NAME)
+change_frame()
+
+# Main program loop. Limited to 30fps by render system
+while True:
+    output = render_screen(boxes, BOX_TYPES, current_frame + 1, NUMBER_OF_FRAMES)
+    if output.find("b#") != -1:
+        index = int(output[2:])
+        if 0 < index <= len(BOX_TYPES):
+            instantiate_box(BOX_TYPES[index - 1][0], BOX_TYPES[index - 1][1], [0,0,0], 0, BOX_TYPES[index - 1][2])
+    elif output.find("f-") != -1 and current_frame >= 1:
+        current_frame -= 1
+        change_frame()
+    elif output.find("f+") != -1 and current_frame < (NUMBER_OF_FRAMES - 1):
+        current_frame += 1
+        change_frame()
+    elif output.find("f#") != -1:
+        index = int(output[2:])
+        if 0 < index <= NUMBER_OF_FRAMES:
+            current_frame = index - 1
+            change_frame()
+    elif output == "output":
+        output_string = ""
+        for box in boxes:
+            output_string += box.to_string() + "\n"
+        f = open(OUTPUT_FILE_NAME,"w")
+        f.write(output_string)
+        f.close()
+        print("Outputted to file " + OUTPUT_FILE_NAME)
--- main.py
+++ main.py
@@ -1,78 +1,56 @@
-from PIL import Image
-from bounding_box import *
-from render import *
-
-##########################################################
-
-# Global constants
-BOX_TYPES = (("Car", (1.7, 2.0, 5.0), (1, 0, 0)),  # (name, dimensions(h,w,l), color_value)
-             ("Cyclist", (1.8,0.5,2.0), (0,1,0)),
-             ("Pedestrian", (1.7,0.5,0.5), (0,0,1)))
-INPUT_FILE_NAME = "image_2/img*.jpg"
-NUMBER_OF_FRAMES = 1500
-OUTPUT_FILE_NAME = "output.txt"
-
-##########################################################
-
-# Sets the background image of the render process to the current frame
-def change_frame():
-    set_background_image(frames[current_frame])
+import pygame
+import numpy as np
 
 
-# Adds a bounding box to the current frame based on several parameters
-def instantiate_box(type, dimensions, location, rotation_y, color_value):
-    # Constructor: (type, truncated, occluded, alpha, bbox, dimensions, location, rotation_y, color_value)
-    new_box = BoundingBox(type, 0, 0, 0, [0,0,0,0], dimensions, location, rotation_y, color_value)
-    global selected_box
-    selected_box = len(boxes)
-    boxes.append(new_box)
+def draw_grid(line_count, distance_between):
+    distance = line_count * distance_between
+    plane_max = distance/2
+    plane_min = -plane_max
+    for i in range(0, line_count + 1):
+        coord = plane_min + i * distance_between
+        draw_line((coord,0,plane_min),(coord,0,plane_max),GRID_COLOR)
+        draw_line((plane_min,0,coord),(plane_max,0,coord),GRID_COLOR)
 
 
-# Loads a given number of frames following a naming convention of type "*.png" where * is replaced by integers from
-# 000001 to number_of_frames (each integer padded to 6 decimal places)
-def setup_frames(number_of_frames, naming_convention):
-    print("Loading " + str(number_of_frames) + " images of type " + naming_convention)
-    for i in range(1,number_of_frames+1):
-        f_index = str(i)
-        while len(f_index) < 6:  # Padding
-            f_index = "0" + f_index
-        pil_image = Image.open(naming_convention.replace("*",f_index))
-        frames.append(pil_image)
-    print ("Loading complete")
-
-# Global variables
-current_frame = 0
-frames = []
-boxes = []
+def draw_line(c1, c2, color):
+    draw_line_screen(get_screen_point(c1),get_screen_point(c2),color)
 
 
-# Initial setup
-setup_frames(NUMBER_OF_FRAMES, INPUT_FILE_NAME)
-change_frame()
+def draw_line_screen(px1, px2, color):
+    pygame.draw.line(render, color, px1, px2, LINE_WIDTH)
 
-# Main program loop. Limited to 30fps by render system
+
+def get_screen_point(point_3d):
+    point_as_matrix = np.array([point_3d[0], point_3d[1], point_3d[2], 1])
+    point = CAMERA_MATRIX.dot(point_as_matrix)
+    point_scaled = np.array([point[0]/point[2],point[1]/point[2],1,1])
+    return (point_scaled[0],point_scaled[1])
+
+
+def render_screen():
+    render.fill([0,0,0])
+    draw_grid(10,1)
+
+
+CAMERA_MATRIX = np.array([[1,0,0,0],
+                          [0,1,0,0],
+                          [0,0,1/3,-2/3],
+                          [0,0,-1/3,1]])
+GRID_COLOR = (0, 0, 255)
+LINE_WIDTH = 3
+RENDER_SIZE = (720, 480)
+
+pygame.init()
+render = pygame.display.set_mode(RENDER_SIZE)
+pygame.display.set_caption("2D graphics test")
+clock = pygame.time.Clock()
+
 while True:
-    output = render_screen(boxes, BOX_TYPES, current_frame + 1, NUMBER_OF_FRAMES)
-    if output.find("b#") != -1:
-        index = int(output[2:])
-        if 0 < index <= len(BOX_TYPES):
-            instantiate_box(BOX_TYPES[index - 1][0], BOX_TYPES[index - 1][1], [0,0,0], 0, BOX_TYPES[index - 1][2])
-    elif output.find("f-") != -1 and current_frame >= 1:
-        current_frame -= 1
-        change_frame()
-    elif output.find("f+") != -1 and current_frame < (NUMBER_OF_FRAMES - 1):
-        current_frame += 1
-        change_frame()
-    elif output.find("f#") != -1:
-        index = int(output[2:])
-        if 0 < index <= NUMBER_OF_FRAMES:
-            current_frame = index - 1
-            change_frame()
-    elif output == "output":
-        output_string = ""
-        for box in boxes:
-            output_string += box.to_string() + "\n"
-        f = open(OUTPUT_FILE_NAME,"w")
-        f.write(output_string)
-        f.close()
-        print("Outputted to file " + OUTPUT_FILE_NAME)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    # Input handler
+    render_screen()
+    pygame.display.flip()
+    clock.tick(30)
